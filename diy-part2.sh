0f24cce53741bf63f922740cd9ca14a42701186d
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-12-18 08:19:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,3 +5,4 @@ sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 mkdir -p files/usr/bin/
 wget https://github.com/Mattaclp/dailook-immortalwrt-mt798x-6.6/releases/download/sing-box-1.12.13-reF1nd-linux-arm64/sing-box
 cp sing-box files/usr/bin/
+chmod +x files/usr/bin/sing-box
