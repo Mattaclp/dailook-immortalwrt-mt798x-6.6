913ecc421b0d91ff9eec4f6d81f45054618312ef
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-12-19 10:56:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,12 +2,12 @@
 sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Use sing-box-1.12.13-reF1nd-linux-arm64
-#mkdir -p files/usr/bin/
-#wget https://github.com/Mattaclp/dailook-immortalwrt-mt798x-6.6/releases/download/sing-box-1.12.13-reF1nd-linux-arm64/sing-box
-#cp sing-box files/usr/bin/
-#chmod +x files/usr/bin/sing-box
+mkdir -p files/usr/bin/
+wget https://github.com/Mattaclp/dailook-immortalwrt-mt798x-6.6/releases/download/sing-box-1.12.13-reF1nd-linux-arm64/sing-box
+cp sing-box files/usr/bin/
+chmod +x files/usr/bin/sing-box
 
 # sing-box依賴刪除
-# sed #sed #sed -i 's/+sing-box//g' feeds/momo/mom
+sed #sed #sed -i 's/+sing-box//g' feeds/momo/mom
 
 rm -rf files/etc/banner
