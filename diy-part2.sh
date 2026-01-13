84d9297d1d6e7b79cb80d6973bc31dd1ad1636da
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2026-01-13 22:50:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,9 +1,9 @@
 # Modify default IP
 sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Use sing-box-1.12.15-reF1nd-linux-arm64
+# Use sing-box-1.12.16-reF1nd-linux-arm64
 mkdir -p files/usr/bin/
-wget https://github.com/Mattaclp/dailook-immortalwrt-mt798x-6.6/releases/download/sing-box-1.12.15-reF1nd-linux-arm64/sing-box
+wget https://github.com/Mattaclp/dailook-immortalwrt-mt798x-6.6/releases/download/sing-box-1.12.16-reF1nd-linux-arm64/sing-box
 cp sing-box files/usr/bin/
 chmod +x files/usr/bin/sing-box
 
