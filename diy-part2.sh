4cd99ef1f31d3521b0c02edb65fe3560b08a8690
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2026-01-17 14:41:51 +0800

    Replace GN version files in diy-part2.sh
    Updated GN version by replacing Makefile and last_commit_position.h.

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,14 @@ sed #sed #sed -i 's/+sing-box//g' feeds/momo/mom
 
 rm -rf files/etc/banner
 
+# replace gn version
+rm -rf feeds/packages/devel/gn/Makefile
+wget https://github.com/Mattaclp/NewLEDE/raw/refs/heads/main/gn/gnMakefile
+mv gnMakefile feeds/packages/devel/gn/Makefile
+rm -rf feeds/packages/devel/gn/src/out/last_commit_position.h
+wget https://github.com/Mattaclp/NewLEDE/raw/refs/heads/main/gn/src/out/last_commit_position.h
+mv last_commit_position.h feeds/packages/devel/gn/src/out/last_commit_position.h
+
 # Zerotier
 #mkdir -p files/etc/config/
 #wget https://raw.githubusercontent.com/Mattaclp/dailook-immortalwrt-mt798x-6.6/refs/heads/main/zerotier
