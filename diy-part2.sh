8f9037a6dd105df21079d003cd5054173825effb
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2026-02-28 23:58:26 +0800

    Disable ci-llvm in rust Makefile

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,5 @@
+sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
+
 # Modify default IP
 sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
