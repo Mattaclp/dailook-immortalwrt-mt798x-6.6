970bebf87786aaf0c45ecc0ce6a036db69abb2a7
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2026-03-05 13:04:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,3 @@
 sed -i '$a src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main' feeds.conf.default
+src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
+src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main
