03c223ae44eac2b6d13e939f1ebe50d49ea13c82
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2026-03-07 16:55:08 +0800

    Create diy-part2.sh

---
+++ diy-part2.sh
@@ -0,0 +1,55 @@
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+
+# 移除 passwall feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+
+# 移除过时的luci-app-passwall版本
+rm -rf feeds/luci/applications/luci-app-passwall
+
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+
+# Rust 1.89.0
+git clone https://github.com/Mattaclp/dailook-immortalwrt-mt798x-6.6.git
+rm -rf feeds/packages/lang/rust/Makefile
+rm -rf feeds/packages/lang/rust/patches/0001-Update-xz2-and-use-it-static.patch
+cp -r dailook-immortalwrt-mt798x-6.6/rust/0001-Update-xz2-and-use-it-static.patch feeds/packages/lang/rust/patches/
+cp -r dailook-immortalwrt-mt798x-6.6/rust/Makefile feeds/packages/lang/rust/
+
+sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
+
+# Modify default IP
+sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# Use sing-box-1.13.0-reF1nd-linux-arm64
+mkdir -p files/usr/bin/
+wget https://github.com/Mattaclp/dailook-immortalwrt-mt798x-6.6/releases/download/sing-box-1.13.0-reF1nd-linux-arm64/sing-box
+cp sing-box files/usr/bin/
+chmod +x files/usr/bin/sing-box
+
+# sing-box依賴刪除
+sed -i 's/+sing-box//g' feeds/momo/momo/Makefile
+
+rm -rf files/etc/banner
+
+# replace gn version
+rm -rf feeds/packages/devel/gn/Makefile
+wget https://github.com/Mattaclp/NewLEDE/raw/refs/heads/main/gn/gnMakefile
+mv gnMakefile feeds/packages/devel/gn/Makefile
+rm -rf feeds/packages/devel/gn/src/out/last_commit_position.h
+wget https://github.com/Mattaclp/NewLEDE/raw/refs/heads/main/gn/src/out/last_commit_position.h
+mv last_commit_position.h feeds/packages/devel/gn/src/out/last_commit_position.h
+
+# Zerotier
+#mkdir -p files/etc/config/
+#wget https://raw.githubusercontent.com/Mattaclp/dailook-immortalwrt-mt798x-6.6/refs/heads/main/zerotier
+#cp zerotier files/etc/config/
+
+# Add OpenClash Meta
+#mkdir -p files/etc/openclash/core
+#wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+#tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
+#mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
+#chmod +x files/etc/openclash/core/clash_meta
+#rm -f "clash_meta.tar.gz"
