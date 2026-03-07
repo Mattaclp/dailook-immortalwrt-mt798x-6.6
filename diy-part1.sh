3163829797b9e1ad4becec6cffbc5e9c6212e925
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2026-03-07 10:01:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,2 @@
 sed -i '$a src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main' feeds.conf.default
-git reset --hard c0cb2126a3afaa2994c43fcdb9e3a08962a2f9fd
+# git reset --hard c0cb2126a3afaa2994c43fcdb9e3a08962a2f9fd
