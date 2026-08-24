d91f9ce2b5c7271b0772368ecffb71cdfe61d7d4
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2026-08-24 11:00:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,3 @@
-sed -i '$a src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main' feeds.conf.default
+# sed -i '$a src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main' feeds.conf.default
 # git reset --hard c0cb2126a3afaa2994c43fcdb9e3a08962a2f9fd
+git clone https://github.com/Slava-Shchipunov/awg-openwrt/ package/awg-openwrt
